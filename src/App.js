0b2ce7f68abf4d0e0edb634c76a5003193c4b464
meiro <mariano.ricardo.eiro@gmail.com>
2022-09-11 09:28:32 -0300

    second coomit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import img0 from './images/rey_ataulfo.png'
+import img1 from './images/rey_ervigio.png'
 
 function App() {
+  
+  function incrementar(e) {
+    e.target.innerHTML = Number(e.target.innerHTML)+1;
+    e.target.style.backgroundColor='green';
+  };
+  
   return (
-    <div className="App">
-      
-    </div>
+      <>
+        <div className='box' onClick={incrementar}>1</div>
+        <button >Accept</button>
+        <div>
+          <img src={img0} />
+        </div>
+        <input className='field'/>
+
+      </>
   );
 }
 
